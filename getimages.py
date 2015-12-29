fa9949620037fcf2d50219379b64b5598ba9b9f1
Shep <brendan.shephard@gmail.com>
2015-12-30 09:49:05 +1000

    Added function to tidy it up

--- getimages.py
+++ getimages.py
@@ -18,7 +18,8 @@ from CUST_INFO import *
 
 ## If there are any spaces this will replace them with '%20' ##
 
-def process_url(raw_url):
+def process_url(CUST_URL):
+    raw_url = CUST_URL
     if ' ' not in raw_url[-1]:
         raw_url=raw_url.replace(' ','%20')
         return raw_url
@@ -26,24 +27,28 @@ def process_url(raw_url):
         raw_url=raw_url[:-1]
         raw_url=raw_url.replace(' ','%20')
         return raw_url
+        checkDir(raw_url)
 
-url=CUST_URL
-parse_object=urlparse(url)
-dirname=basename(parse_object.path)
-if not os.path.exists('images'):
-os.mkdir("images/"+(CUST_NAME))
-os.chdir("images/"+(CUST_NAME))
+def checkDir(raw_url):
+    url = raw_url
+    parse_object = urlparse(url)
+    dirname = basename(parse_object.path)
+    if not os.path.exists('images'):
+    os.mkdir("images/"+(CUST_NAME))
+    os.chdir("images/"+(CUST_NAME))
+    collectImg(url)
 
-urlcontent=urllib2.urlopen(url).read()
-imgurls=re.findall('img .*?src="(.*?)"',urlcontent)
-for imgurl in imgurls:
-    try:
-        imgurl=process_url(imgurl)
-        imgdata=urllib2.urlopen(imgurl).read()
-        filname=basename(urlsplit(imgurl)[2])
-        output=open(filname,'wb')
-        output.write(imgdata)
-        output.close()
-        os.remove(filename)
-    except:
-        pass
+def collectImg(url):
+    urlcontent = urllib2.urlopen(url).read()
+    imgurls = re.findall('img .*?src="(.*?)"',urlcontent)
+    for imgurl in imgurls:
+        try:
+            imgurl = process_url(imgurl)
+            imgdata = urllib2.urlopen(imgurl).read()
+            filename = basename(urlsplit(imgurl)[2])
+            output = open(filename,'wb')
+            output.write(imgdata)
+            output.close()
+            os.remove(filename)
+        except:
+            pass
