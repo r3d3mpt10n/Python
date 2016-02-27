242a1f264d7b975dd3a19be90c15ec780a36766c
Shep <shep@Fedora.Macbook>
2016-02-27 21:23:51 +1000

    Created BackUp class to make this script more portable.

--- homeBackup.py
+++ homeBackup.py
@@ -6,47 +6,53 @@ import tarfile
 import os
 import datetime
 
-now = datetime.datetime.now()
-date = str(now)
-BDIR = '/media/Storage/Server.Backup/home_backups/'  # ADD YOUR BACKUP DIRECTORY HERE BEFORE RUNNING
-DIR = '/home/shep/'  # ADD YOUR HOME DIR HERE BEFORE RUNNING
-BFILE = BDIR + 'Home.backup' + date + 'tar.bz2'
+class BackUp(object):
 
-delta = datetime.timedelta(30)
-now30 = now - delta
+    def __init__(self):
 
+        now = datetime.datetime.now()
+        date = str(now)
+        BDIR = '/media/Storage/Server.Backup/home_backups/'  # ADD YOUR BACKUP DIRECTORY HERE BEFORE RUNNING
+        DIR = '/home/shep/'  # ADD YOUR HOME DIR HERE BEFORE RUNNING
+        BFILE = BDIR + 'Home.backup' + date + 'tar.bz2'
 
-
-def check(BDIR,DIR):
-    try:
-        exists = os.path.isdir(BDIR)
-        runBackup(BFILE, DIR)
-    except OSError as e:
-        print("'%s' Does not exist. This program will now exit\n "
-               "Error is %s" % (BDIR, e))
-    return(exists)
+        delta = datetime.timedelta(30)
+        now30 = now - delta
+        self.check(BDIR, DIR, BFILE)
 
 
 
-
-def runBackup(BFILE, DIR):
-
-    with tarfile.open(BFILE, 'w:bz2') as tar:
-        tar.add(DIR, arcname=os.path.basename(DIR))
-    removeOld()
-
-
-def removeOld():
-    for name in os.listdir(BDIR):
-        fname = os.path.join(BDIR, name)
-        if os.path.isfile(fname):
-            t = os.path.getmtime(fname)
-            dt = datetime.datetime.utcfromtimestamp(t)
-            if dt <= now30:
-                print('Deleting', repr(fname))
-            # os.remove(fname)  # This is commented out while debugging.
-            else:
-                print (repr(fname), '  are not old enough to not be deleted')
+    def check(self, BDIR, DIR, BFILE):
+        try:
+            exists = os.path.isdir(BDIR)
+            self.runBackup(BFILE, DIR)
+        except OSError as e:
+            print("'%s' Does not exist. This program will now exit\n "
+                "Error is %s" % (BDIR, e))
+        return(exists)
 
 
-check(BDIR, DIR)
+
+
+    def runBackup(self, BFILE, DIR):
+
+        with tarfile.open(BFILE, 'w:bz2') as tar:
+            tar.add(DIR, arcname=os.path.basename(DIR))
+        self.removeOld()
+
+
+    def removeOld(self, BDIR):
+        for name in os.listdir(BDIR):
+            fname = os.path.join(BDIR, name)
+            if os.path.isfile(fname):
+                t = os.path.getmtime(fname)
+                dt = datetime.datetime.utcfromtimestamp(t)
+                if dt <= now30:
+                    print('Deleting', repr(fname))
+                # os.remove(fname)  # This is commented out while debugging.
+                else:
+                    print (repr(fname), '  are not old enough to not be deleted')
+
+
+BackUp()
+
