6554df157551f6c9bdf2710db797e7a4bfd4c93b
Shep <shep@Fedora.Macbook>
2016-02-27 21:33:26 +1000

    Class will request directory's to backup to and to be backed up

--- homeBackup.py
+++ homeBackup.py
@@ -12,12 +12,12 @@ class BackUp(object):
 
         now = datetime.datetime.now()
         date = str(now)
-        BDIR = '/media/Storage/Server.Backup/home_backups/'  # ADD YOUR BACKUP DIRECTORY HERE BEFORE RUNNING
-        DIR = '/home/shep/'  # ADD YOUR HOME DIR HERE BEFORE RUNNING
-        BFILE = BDIR + 'Home.backup' + date + 'tar.bz2'
+        BDIR = input("Which directory would you like to backup too?\n ")
+        DIR = input("Which directory would you like to create a backup of?\n ")
+        BFILE = BDIR + '/Home.backup' + date + 'tar.bz2'
 
         delta = datetime.timedelta(30)
-        now30 = now - delta
+        self.now30 = now - delta
         self.check(BDIR, DIR, BFILE)
 
 
@@ -25,7 +25,7 @@ class BackUp(object):
     def check(self, BDIR, DIR, BFILE):
         try:
             exists = os.path.isdir(BDIR)
-            self.runBackup(BFILE, DIR)
+            self.runBackup(BFILE, DIR, BDIR)
         except OSError as e:
             print("'%s' Does not exist. This program will now exit\n "
                 "Error is %s" % (BDIR, e))
@@ -34,11 +34,11 @@ class BackUp(object):
 
 
 
-    def runBackup(self, BFILE, DIR):
+    def runBackup(self, BFILE, DIR, BDIR):
 
         with tarfile.open(BFILE, 'w:bz2') as tar:
             tar.add(DIR, arcname=os.path.basename(DIR))
-        self.removeOld()
+        self.removeOld(BDIR)
 
 
     def removeOld(self, BDIR):
@@ -47,7 +47,7 @@ class BackUp(object):
             if os.path.isfile(fname):
                 t = os.path.getmtime(fname)
                 dt = datetime.datetime.utcfromtimestamp(t)
-                if dt <= now30:
+                if dt <= self.now30:
                     print('Deleting', repr(fname))
                 # os.remove(fname)  # This is commented out while debugging.
                 else:
