ebb21a8a61c5fb36bae2fc3d26f6f7546122dc7b
Shep <shep@Fedora.Macbook>
2016-03-20 20:15:12 +1000

    Updated homeBackup.py

--- homeBackup.py
+++ homeBackup.py
@@ -9,41 +9,52 @@ import datetime
 class BackUp(object):
 
     def __init__(self):
+        """
+        __init__ will define the variables to be used by the class. First by setting the directory to be backed up and defining time variables.
+        """
 
-        now = datetime.datetime.now()
-        date = str(now)
-        BDIR = input("Which directory would you like to backup too?\n ")
-        DIR = input("Which directory would you like to create a backup of?\n ")
-        BFILE = BDIR + "/Home.backup" + date + "tar.bz2"
+        self.now = datetime.datetime.now()
+        self.date = str(self.now)
+        self.BDIR = input("Which directory would you like to backup too?\n ")
+        self.DIR = input("Which directory would you like to create a backup of?\n ")
+        self.BFILE = self.BDIR + "/Home.backup" + self.date + "tar.bz2"
 
-        delta = datetime.timedelta(30)
-        self.now30 = now - delta
-        self.check(BDIR, DIR, BFILE)
+        self.delta = datetime.timedelta(30)
+        self.now30 = self.now - self.delta
+        
 
 
 
-    def check(self, BDIR, DIR, BFILE):
+    def check(self):
+        """
+        Check the directory exists
+        """
         try:
-            exists = os.path.isdir(BDIR)
-            self.runBackup(BFILE, DIR, BDIR)
+            exists = os.path.isdir(self.BDIR)
         except OSError as e:
             print("'%s' Does not exist. This program will now exit\n "
-                "Error is %s" % (BDIR, e))
+                "Error is %s" % (self.BDIR, e))
         return(exists)
 
 
 
 
-    def runBackup(self, BFILE, DIR, BDIR):
+    def runBackup(self):
 
-        with tarfile.open(BFILE, 'w:bz2') as tar:
-            tar.add(DIR, arcname=os.path.basename(DIR))
-        self.removeOld(BDIR)
+        """
+        Creates a tar.bz2 file backup of the specified directory
+        """
+        with tarfile.open(self.BFILE, 'w:bz2') as tar:
+            tar.add(self.DIR, arcname=os.path.basename(self.DIR))
+        
 
 
-    def removeOld(self, BDIR):
-        for name in os.listdir(BDIR):
-            fname = os.path.join(BDIR, name)
+    def removeOld(self):
+        """
+        Checks for old backup, removes backups older than 30 days
+        """
+        for name in os.listdir(self.BDIR):
+            fname = os.path.join(self.BDIR, name)
             if os.path.isfile(fname):
                 t = os.path.getmtime(fname)
                 dt = datetime.datetime.utcfromtimestamp(t)
@@ -54,5 +65,7 @@ class BackUp(object):
                     print ("%s are not old enough to not be deleted" % (fname))
 
 
-BackUp()
 
+
+new = BackUp()
+print(new.check())
